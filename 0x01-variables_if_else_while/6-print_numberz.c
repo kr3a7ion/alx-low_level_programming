33e4c55d94fd5a78578fa03c626abda31b47a962
kr3a7ion <kr3a7ion@gmail.com>
2023-06-17 20:09:13 +0100

    low level programming if else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the single digit numbers
+ *
+ * Description: print the single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
